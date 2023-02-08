fdce7538d6b050d4b00a16a06cce18c8d64c66e3
Dr. K.D. Murray <foss@kdmurray.id.au>
2023-02-08 17:46:01 +0100

    use version from _version.py

--- CAT/about.py
+++ CAT/about.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+from CAT._version import version
 __author__ = 'F. A. Bastiaan von Meijenfeldt'
-__version__ = '5.2.3'
+__version__ = version
 __date__ = '10 February, 2021'
